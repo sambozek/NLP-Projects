281c4ba6372802dd622334262a17b8a3ec750510
Sam Bozek <sambozek@gmail.com>
2016-08-09 20:35:27 -0700

    Adjusted index for proper reference

--- play_me_out/src/index.js
+++ play_me_out/src/index.js
@@ -14,8 +14,8 @@
  *
  * Examples:
  * One-shot model:
- *  User: "Alexa, ask Space Geek for a space fact"
- *  Alexa: "Here's your space fact: ..."
+ *  User: "Alexa, ask Space Geek for a space Ragtime"
+ *  Alexa: "Here's your space Ragtime: ..."
  */
 
 /**
@@ -24,22 +24,12 @@
 var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 
 /**
- * Array containing space facts.
+ * Array containing Ragtimes.
  */
-var FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+var RAGTIME = [
+    '<audio src="https://s3.amazonaws.com/playmeout/entertainer30s.mp3" />',
+    '<audio src="https://s3.amazonaws.com/playmeout/frogleg30s.mp3" />',
+    '<audio src="https://s3.amazonaws.com/playmeout/mapleleaf30s.mp3" />',
 ];
 
 /**
@@ -53,39 +43,39 @@ var AlexaSkill = require('./AlexaSkill');
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var Fact = function () {
+var Ragtime = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
-Fact.prototype = Object.create(AlexaSkill.prototype);
-Fact.prototype.constructor = Fact;
+Ragtime.prototype = Object.create(AlexaSkill.prototype);
+Ragtime.prototype.constructor = Ragtime;
 
-Fact.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+Ragtime.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
     //console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
-Fact.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+Ragtime.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
     //console.log("onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-    handleNewFactRequest(response);
+    handleNewRagtimeRequest(response);
 };
 
 /**
  * Overridden to show that a subclass can override this function to teardown session state.
  */
-Fact.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+Ragtime.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
     //console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
-Fact.prototype.intentHandlers = {
-    "GetNewFactIntent": function (intent, session, response) {
-        handleNewFactRequest(response);
+Ragtime.prototype.intentHandlers = {
+    "GetRagtimeIntent": function (intent, session, response) {
+        handleNewRagtimeRequest(response);
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        response.ask("You can say tell me a space fact, or, you can say exit... What can I help you with?", "What can I help you with?");
+        response.ask("You can ask for ragtime, or, you can say cancel.");
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
@@ -94,29 +84,27 @@ Fact.prototype.intentHandlers = {
     },
 
     "AMAZON.CancelIntent": function (intent, session, response) {
-        var speechOutput = "Goodbye";
+        var speechOutput = "Canceling";
         response.tell(speechOutput);
     }
 };
 
 /**
- * Gets a random new fact from the list and returns to the user.
+ * Gets a random new Ragtime from the list and returns to the user.
  */
-function handleNewFactRequest(response) {
-    // Get a random space fact from the space facts list
-    var factIndex = Math.floor(Math.random() * FACTS.length);
-    var randomFact = FACTS[factIndex];
+function handleNewRagtimeRequest(response) {
+    // Get a random space Ragtime from the space Ragtimes list
+    var ragIndex = Math.floor(Math.random() * RAGTIME.length);
+    var randomRag = Ragtime[ragIndex];
 
     // Create speech output
-    var speechOutput = "Here's your fact: " + randomFact;
-    var cardTitle = "Your Fact";
-    response.tellWithCard(speechOutput, cardTitle, speechOutput);
+    var speechOutput = randomRag;
 }
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
     // Create an instance of the SpaceGeek skill.
-    var fact = new Fact();
-    fact.execute(event, context);
+    var ragtime = new Ragtime();
+    ragtime.execute(event, context);
 };
 
